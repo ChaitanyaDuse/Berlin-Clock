9cb3240aa9447cddc1309530328d491a97c19b86
chaitanyaduse <chaitanyaduse31@gmail.com>
2024-04-08 22:29:24 +0530

    launcher icon added

--- app/src/main/java/com/debunrebuild/berlinclock/StandardClock.kt
+++ app/src/main/java/com/debunrebuild/berlinclock/StandardClock.kt
@@ -1,13 +1,16 @@
 package com.debunrebuild.berlinclock
 
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Locale
 
 
 class StandardClock {
-
+    val stringFormat = "hh:mm:ss"
     fun setTime(time: Long) {
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = time
+        timeInMiliSeconds = time
         this.hour = calendar.get(Calendar.HOUR_OF_DAY)
         this.minute = calendar.get(Calendar.MINUTE)
         this.second = calendar.get(Calendar.SECOND)
@@ -16,8 +19,9 @@ class StandardClock {
     var hour = 0
     var minute = 0
     var second = 0
+    var timeInMiliSeconds : Long = 0
 
     override fun toString(): String {
-        return "$hour : $minute : $second"
+        return SimpleDateFormat(stringFormat, Locale.getDefault(Locale.Category.FORMAT)).format(timeInMiliSeconds)
     }
 }
